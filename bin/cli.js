e226532791b22b140cc04e6d0a1bdf36015c2a88
hakovala <hakovala@gmail.com>
2012-12-13 01:12:43 +0200

    Api Actions system added. Most of the Api calls work.

--- bin/cli.js
+++ bin/cli.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 var Trakt = require('../lib/index.js');
 var argv = require('optimist')
-	.usage('Usage: $0 search <show name>')
+	.usage('Usage: $0 -a <action> -m <method> [options]')
 	.alias('u', 'user')
 	.alias('p', 'pass')
-	.alias('t', 'type')
+	.alias('a', 'action')
+	.alias('m', 'method')
+	.demand(['a','m'])
 	.argv;
 
 var trakt = new Trakt({user: argv.user, password: argv.pass}); 
@@ -13,87 +15,15 @@ trakt.on('error', function(err) {
 	console.log("Trakt error: " + err.message)
 })
 
-
-/*
- * TODO ::: THIS IS BROKEN
- */
-trakt.on('ready', function(result) {
-	switch (result.op) {
-		case 'search':
-			if (result.type == 'shows') {
-				result.results.forEach(function(show) {
-					console.log(show.title);
-				})
-			} else if (result.type == 'episodes') {
-				result.results.forEach(function(res) {
-					console.log(res.show.title);
-					console.log(res.episode.title)
-				})
-			} else if (result.type == 'movies') {
-				result.results.forEach(function(movie) {
-					console.log(movie.title);
-				})
-			}
-		break;
-	case 'activity':
-		console.log(result.results);
-		break;
-	case 'server_time':
-		console.log(result.results);
-		break;
-	case 'genres':
-		console.log(result.results);
-		break;
-	default:
-		console.warn("Unknown result: " + result.op);
-		break;
+trakt.request(argv.action, argv.method, argv, function(err, result) {
+	result = JSON.stringify(result, null, 4)
+	if (err) {
+		console.log(err);
+		if (result) {
+			console.log(result);
+		}
+	} else {
+		console.log(result);
 	}
 })
-
-if (argv.test) {
-	argv._.splice(1, 99,
-		"continuum",
-		"american dad",
-		"fringe",
-		"family guy",
-		"top gear",
-		"top chef",
-		"whose line",
-		"horizon"
-	)
-}
-
-
-// Check cli arguments
-switch (argv._[0]) {
-	case 'search':
-		var type = argv.type ? argv.type : 'shows';
-		trakt.search(type, argv._.slice(1));
-		break;
-	case 'account':
-		trakt.account('test')
-		trakt.account('settings')
-		break;
-	case 'activity':
-		trakt.activity({cmd: 'community'})
-		trakt.activity({cmd: 'episodes'})
-		trakt.activity({cmd: 'friends'})
-		trakt.activity({cmd: 'movies'})
-		trakt.activity({cmd: 'seasons'})
-		trakt.activity({cmd: 'shows'})
-		trakt.activity({cmd: 'user'})
-		break;
-	case 'server_time':
-		trakt.server_time();
-		break;
-	case 'genres':
-		trakt.genres({cmd: 'movies'})
-		trakt.genres({cmd: 'shows'})
-		break;
-	default: 
-		console.log("Unknown command")
-		break;
-}
-
-
 
