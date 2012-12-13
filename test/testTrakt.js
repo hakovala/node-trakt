7ba6eef28a6832f487c26eb6b6bc369e0a00bb00
hakovala <hakovala@gmail.com>
2012-12-13 20:40:19 +0200

    TestCases updated

--- test/testTrakt.js
+++ test/testTrakt.js
@@ -1,5 +1,9 @@
 var should = require('should')
+var fs = require('fs')
 var Trakt = require('../index.js');
+
+
+var config = JSON.parse(fs.readFileSync('test/test_config.json', 'utf8'))
 
 describe('Trakt init', function(){
 	it('should be valid Trakt object', function(){
@@ -11,5 +15,44 @@ describe('Trakt init', function(){
 			.should.be.an.instanceof(Trakt)
 		new Trakt({username: 'test', password: 'test', pass_hash: true})
 			.should.be.an.instanceof(Trakt)
+	})
+})
+
+describe('Trakt requests', function() {
+	var trakt = new Trakt()
+	describe('Get request', function() {
+		it('should give error, missing param', function() {
+			trakt.request('search', 'shows', {}, function(err, res) {
+				should.exist(err)
+			})
+		})
+		it('should be ok', function() {
+			trakt.request('search', 'shows', {query: 'hello'}, function(err, res) {
+				should.not.exist(err)
+				should.exist(res)
+			})
+		})
+	})
+	describe('Post request', function() {
+		it('need user and password, specified in test_config.json', function() {
+			should.exist(config)
+			config.should.have.property('user')
+			config.user.should.not.equal('TEST USER')
+		})
+		it('should give error, no auth', function() {
+			trakt.request('account', 'test', {}, function(err, res) {
+				should.exist(err)
+			})
+		})
+		it('should be ok', function() {
+			trakt.setUser(config.user, config.pass)
+			trakt.request('account', 'test', {}, function(err, res) {
+				should.not.exist(err)
+				should.exist(res)
+				res.should.have.property('status')
+				res.should.have.property('message')
+				res.status.should.equal('success')
+			})
+		})
 	})
 })
