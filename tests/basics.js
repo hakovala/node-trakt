f8b38a503bcf526a2d497f475875da2ecab76f79
hakovala <hakovala@gmail.com>
2012-12-13 03:19:04 +0200

    Basic unit tests and coverage reporting added

--- tests/basics.js
+++ tests/basics.js
@@ -1,7 +1,18 @@
-module.exports = {
-	'Test 1' : function(test) {
-		test.expect(1)
-		test.ok(true, 'This should not fail')
-		test.done()
-	}
-}
+var test = require('tap').test 
+var Trakt = require('..')
+
+test("creating Trakt object", function(t) {
+	var trakt = new Trakt()
+	t.ok(trakt instanceof Trakt, 'Trakt without options')
+	trakt = new Trakt({api_key: 'dummy'})
+	t.ok(trakt instanceof Trakt, 'Trakt with api_key')
+	trakt = new Trakt({username: 'test', password: 'test', api_key: 'dummy'})
+	t.ok(trakt instanceof Trakt, 'Trakt with username, password and api_key')
+	trakt = new Trakt({username: 'test', password: 'test', pass_hash: true})
+	t.ok(trakt instanceof Trakt, 'password is already hash')
+	t.end()
+});
+
+test("request testing", function(t) {
+	t.end()	
+});
